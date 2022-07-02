c4ec252bf049e08106750c0a969779c95a9ba1da
fedechaves <federicochavesf@gmail.com>
2022-07-01 21:11:10 -0400

    changes to our DB builder

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 //Require dependencies
-
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = 8008
+const PORT = 8000
 require('dotenv').config()
 
 //Declare DB Variables
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'star-trek-api'
+
 //Connect to Mongo
-MongoClient.connect(dbConnectionStr)
-    .catch(client => {
-        console.log(`Connectedto ${dbName} Database`)
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
         })
+
 
 //Set Middlewares
 app.set('view engine', 'ejs')
@@ -23,8 +24,62 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.use('/', (req, res) => {
-    res.sendFile(__dirname + '/index.ejs')
+//CRUD METHODS
+
+app.get('/', (req, res) => {
+    db.collection('star-trek-info').find().toArray()
+        .then(data => {
+            let nameList = data.map(item => item.speciesName)
+            console.log(nameList)
+            res.render('index.ejs', {info: nameList })
+        })
+        .catch(error => console.log(error))
+
+})
+
+app.post('/api', (req, res) => {
+    console.log('port heard')
+    db.collection('star-trek-info').insertOne(
+        req.body
+    )
+    .then(result => {
+        console.log(result)
+        res.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+//Does not function yet
+app.post('/massinsert', (req, res) => {
+
+})
+
+app.put('/updateEntry', (req, res) => {
+    console.log(req.body)
+    Object.keys(req.body).forEach(key=> {
+        if(req.body[key] == null || req.body[key] === undefined || req.body[key]=== ''){
+            delete req.body[key];
+        }
+    });
+    console.log(req.body)
+    db.collection('star-trek-info').findOneAndUpdate(
+        {name: req.body.name},
+        {
+            $set:  req.body  
+        },
+        // {
+        //     upsert: true
+        // }
+    )
+    .then(result => {
+        console.log(result)
+        res.json('Success')
+    })
+
+})
+
+app.delete('/deleteEntry', (req, res) =>{
+
 })
 
 app.listen(process.env.PORT || PORT, () => {
